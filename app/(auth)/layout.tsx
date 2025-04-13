e45773fb68c317ea2cfcbf04fdcad11b661562ea
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-04-13 12:04:01 +0530

    added client components, Used conditional rendering in layout.jsx, rectified hydration errors using client component as "use client"

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,12 @@ import Navbar from "../components/Navbar";
 export default function AuthLayout({ children }) {
   return (
     <div>
-      <Navbar />
+      {/* let time = new Date().getTime()*/}
+      {Math.random() < 0.5 && "Hi"}{" "}
+      {/* put it in useEffect for consistent behaviour
+      otherwise we get the hydration error if threshold value reaches above 0.5 like 0.6 (Hi randomly with 50% probability.) -> use "use client" -> This avoid mismatch
+      problem will raise ssr hydration errors
+      0.5 is a probability threshold*/}
       {children}
     </div>
   );
