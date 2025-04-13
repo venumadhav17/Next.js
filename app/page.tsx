be902e86086f26e0ca9d0d951f8534baccefeee2
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-04-13 13:06:47 +0530

    used <pre> t rectify hydration error, used json placeholder data, axios

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import axios from "axios";
+
+async function getAllBlogs() {
+  const res = await axios("https://jsonplaceholder.typicode.com/todos/");
+  return res.data;
 }
+
+export default async function Blogs() {
+  const blogs = await getAllBlogs();
+
+  return <pre>{JSON.stringify(blogs)}</pre>; // unstable hydration // {data,null,2}
+}
+// Never use "use client" in components with await
+// use <pre> for conistency</pre>
+// Hydration error = mismatch between server HTML and client React tree
